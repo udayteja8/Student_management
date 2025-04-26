b2eb83abc2d007aca3a5a8d8ca29850ef0d63e09
udayteja8 <udayteja846@gmail.com>
2025-04-27 01:44:15 +0530

    Update apiService.js

--- frontend/src/apiService.js
+++ frontend/src/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/Students';
+const API_URL = 'https://student-management-1-5pie.onrender.com/api/Students';
 // Create axios instance
 const api = axios.create({
     baseURL: API_URL
